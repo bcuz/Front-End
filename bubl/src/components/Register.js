b3685871701c7f62de7dbbcfd9b8175604219f23
adam <3193391+bcuz@users.noreply.github.com>
2020-06-13 14:49:47 -0400

    simplify regsiter form

--- bubl/src/components/Register.js
+++ bubl/src/components/Register.js
@@ -39,18 +39,10 @@ class Register extends Component {
          <Form onSubmit={this.handleSubmit}>
             <h2>Register</h2>
             {this.props.registerError && <p>Error on sign up, try again</p>}
-            <Label htmlFor="firstName">First Name:</Label>
-            <Input name="firstName" value={this.state.newUser.firstName} onChange={this.handleChange} placeholder="First Name" />
-            <Label htmlFor="lastName">Last Name:</Label>
-            <Input name="lastName" value={this.state.newUser.lastName} onChange={this.handleChange} placeholder="Last Name" />
             <Label htmlFor="username">Username:</Label>
             <Input name="username" value={this.state.newUser.username} onChange={this.handleChange} placeholder="Username" />
             <Label htmlFor="password">Password:</Label>
-            <Input name="password" type='password' value={this.state.newUser.password} onChange={this.handleChange} placeholder="Password" />
-            <Label htmlFor="age">Age:</Label>
-            <Input name="age" type='number' value={this.state.newUser.age} onChange={this.handleChange} placeholder="Age" />
-            <Label htmlFor="school_id">School:</Label>
-            <Input name="school_id" type='number' value={this.state.newUser.school_id} onChange={this.handleChange} placeholder="School" />
+            <Input name="password" type='password' value={this.state.newUser.password} onChange={this.handleChange} placeholder="Password" />            
             <LoginButton>Register</LoginButton>
          </Form>
       )
