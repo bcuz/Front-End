b28081c6e8aba7de911beea54f898c4e25b602a1
adam <3193391+bcuz@users.noreply.github.com>
2020-07-11 13:45:07 -0400

    basic endpoint to hit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = require("./api/server");
 
 const port = process.env.PORT || 5000;
 
-server.get("/", (req, res) => {
+server.get("/hi", (req, res) => {
   res.send("hi");
 });
 server.listen(port, () => console.log(`listening to ${port}`));
