0fd4cd243e81e9f93c31575ba020969cee35b203
adam <3193391+bcuz@users.noreply.github.com>
2020-07-15 12:22:40 -0400

    use knex cleaner

--- data/seeds/01-bubl.js
+++ data/seeds/01-bubl.js
@@ -1,8 +1,8 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('bubl').del()
-    .then(function () {
+  // return knex('bubl').del()
+  //   .then(function () {
       // Inserts seed entries
       return knex('bubl').insert([
         {id: 1, bublName: 'pokemon'},
@@ -12,5 +12,5 @@ exports.seed = function(knex, Promise) {
         {id: 5, bublName: 'video games'},
         {id: 6, bublName: 'football'}
       ]);
-    });
+    // });
 };
